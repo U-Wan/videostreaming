999af4d9cd5f10f530e588f1c1ae03c4b54ce42c
Zurab Dzindzibadze <zuradzindzibadze06@gmail.com>
2022-06-28 15:21:14 +0400

    added logout

--- app/src/main/java/com/tsu/videostreaming/WelcomeActivity.kt
+++ app/src/main/java/com/tsu/videostreaming/WelcomeActivity.kt
@@ -3,7 +3,11 @@ package com.tsu.videostreaming
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
 import com.tsu.videostreaming.agora.StreamingActivity
 
 class WelcomeActivity : AppCompatActivity() {
@@ -19,6 +23,23 @@ class WelcomeActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.go_to_Signin -> {
+                FirebaseAuth.getInstance().signOut()
+                val intent = Intent(this, SignInActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 
